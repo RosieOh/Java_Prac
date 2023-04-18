18c280654038efdc52ac9954fe3d27fcf8cc48e7
RosieOh <20172207@gm.hannam.ac.kr>
2023-04-19 00:58:18 +0900

    자바 객체지향프로그래밍 연습

---
+++ src/DoWhileExam2.java
@@ -0,0 +1,12 @@
+public class DoWhileExam2 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i < 1);
+    }
+}
+
+// do while문은 사용자에게 한번은 값을 입력 받고 싶을 때, 그리고 입력 받은 값이 원하지 않은 값일 경우
+// 바로 종료 되고 싶을 때 사용될 수 있을 것.
